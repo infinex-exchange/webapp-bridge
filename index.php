b8d1fa4b132018dea7a00fca4b8a600e46d95922
vayamos-exchange <admin@vayamos.cc>
2022-07-16 11:41:03 +0200

    Button colors

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                                 <button type="button" class="btn btn-primary w-100 toggle-peg" data-side="peg-in">Peg-in</button>
                             </div>
                             <div class="col-6 ps-1 pe-2">
-                                <button type="button" class="btn btn-muted w-100 toggle-peg" data-side="peg-out">Peg-out</button>
+                                <button type="button" class="btn btn-secondary w-100 toggle-peg" data-side="peg-out">Peg-out</button>
                             </div>
                         </div>
                         <div class="row py-2 text-center">
