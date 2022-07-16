64a2db0940d8e15c1df670c8d41aa7614a9162a9
vayamos-exchange <admin@vayamos.cc>
2022-07-16 16:32:01 +0200

    resize separator

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                             </p>
                         </div>
                         <div class="col-8 d-none d-lg-block mx-auto">
-                            <div style="height: 100px"></div>
+                            <div style="height: 80px"></div>
                             <img src="/bridge/img/bridge.png" class="img-fluid">
                         </div>
                     </div>
