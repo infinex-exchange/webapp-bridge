25a3adad09a78ed5ae983b9c39179c529ba0728a
vayamos-exchange <admin@vayamos.cc>
2022-07-16 11:44:30 +0200

    Toggle button js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
     window.renderingStagesTarget = 1;
     $(document).trigger('renderingStage');
+    
+    $('.toggle-peg').on('click', function() {
+        var side = $(this).data('side');
+        
+        if(side == 'peg-in') {
+            //
+        }
+        
+        $('.toggle-peg').toggleClass('btn-primary btn-secondary');
+    });
 });
