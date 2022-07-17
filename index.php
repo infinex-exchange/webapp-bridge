37f71696506b8e119e6172950bf9b86d00662d87
vayamos-exchange <admin@vayamos.cc>
2022-07-17 11:07:56 +0200

    resize column

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <div id="root" class="container-fluid container-1500 h-rest pt-2 p-0">
         
             <div class="row h-rest m-0">
-                <div class="col-12 col-lg-7 my-auto px-4 py-5">
+                <div class="col-12 col-lg-6 me-auto my-auto px-4 py-5">
                     <div class="row">
                         <div class="col-12 jumbotron">
                             <h1>Vayamos Bridge</h1>
