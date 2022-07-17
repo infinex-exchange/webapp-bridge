debcb554d68754d93d214472d4c6dd8cd847ae82
vayamos-exchange <admin@vayamos.cc>
2022-07-17 15:58:55 +0200

    Style step2

--- index.php
+++ index.php
@@ -112,8 +112,9 @@
                         <div id="bridge-step2">
                             <div class="row py-2 flex-nowrap justify-content-evenly">
                                 <div class="col-auto text-center">
-                                    <div class="p-1 rounded" style="background-color: var(--color-input);">
+                                    <div class="p-2 rounded text-center" style="background-color: var(--color-input);">
                                         <img id="peg-from-net-img" width="40" height="40" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
+                                        <br>
                                         <span id="peg-from-net" class="p-1 secondary"></span>
                                     </div>
                                 </div>
@@ -121,8 +122,9 @@
                                     <i class="fa-solid fa-arrow-right fa-2x"></i>
                                 </div>
                                 <div class="col-auto text-center">
-                                    <div class="p-1 rounded" style="background-color: var(--color-input);">
+                                    <div class="p-2 rounded text-center" style="background-color: var(--color-input);">
                                         <img id="peg-to-net-img" width="40" height="40" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
+                                        <br>
                                         <span id="peg-to-net" class="p-1 secondary"></span>
                                     </div>
                                 </div>
