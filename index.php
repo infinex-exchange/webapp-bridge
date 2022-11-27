ec43396b23f55c77096bc700fd738f8d40342196
vayamos-exchange <admin@vayamos.cc>
2022-11-27 11:07:03 +0100

    Rebranding

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <?php include(__DIR__.'/../../templates/head.php'); ?>
         <script src="/js/ajax_scroll.js?<?php echo filemtime(__DIR__.'/../../js/ajax_scroll.js'); ?>"></script>
-        <title>Bridge - Vayamos Exchange</title>
+        <title>Bridge - Infinex</title>
     </head>
     <body class="body-background">
     
@@ -20,9 +20,9 @@
                 <div class="col-12 col-lg-6 me-auto my-auto px-4 py-5">
                     <div class="row">
                         <div class="col-12">
-                            <h1>Vayamos Bridge</h1>
+                            <h1>Infinex Bridge</h1>
                             <p>
-                                With Vayamos Bridge you can wrap any asset supported by Vayamos Exchange to BPX blockchain tokens.
+                                With Infinex Bridge you can wrap any asset supported by Infinex to BPX blockchain tokens.
                                 You can hold your wrapped tokens in your BPX wallet, trade them using offers and transfer them without fee.
                                 BPX tokens are pegged on a 1:1 basis to the underlying asset.
                                 You can redeem your wrapped tokens back to the original asset at any time.
